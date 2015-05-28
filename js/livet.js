8d9bd9f253080ed3a459d91be23d9f1950b789be
Dion Santana <dion.santana@gmail.com>
2015-05-28 11:58:48 -0400

    resimplify

--- js/livet.js
+++ js/livet.js
@@ -1,6 +1,9 @@
 
-// App Logic
-function appLogic()
-{	
-	alert('hurray');	
+//Catch "deviceready" event which is fired when PhoneGap is ready
+document.addEventListener("deviceReady", deviceReady, false);
+
+//Resolve gapReady in reponse to deviceReady event
+function deviceReady()
+{
+	alert('hurray');
 }
