e169a5cad356ff20c739f342c3d7e7ab200442aa
Dion Santana <dion.santana@gmail.com>
2015-05-28 14:51:09 -0400

    alert after

--- js/livet.js
+++ js/livet.js
@@ -33,7 +33,7 @@ $.when(gapReady, jqmReady).then(myAppLogic);
 // App Logic
 function myAppLogic()
 {
-	alert( 'hurray' );
 	$( "#tour" ).popup();
 	$( "#tour" ).popup( "open" );
+	alert( "hurray" );	
 }
