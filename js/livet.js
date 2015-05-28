58909d6ebee4f3681665570f8c935215c4a3dd07
Dion Santana <dion.santana@gmail.com>
2015-05-28 16:12:09 -0400

    stress man

--- js/livet.js
+++ js/livet.js
@@ -1,7 +1,7 @@
-//Catch "deviceready" event which is fired when PhoneGap is ready
+
 document.addEventListener("deviceReady", deviceReady, false);
 
-//Resolve gapReady in reponse to deviceReady event
+
 function deviceReady()
 {
 	alert('sigh');
