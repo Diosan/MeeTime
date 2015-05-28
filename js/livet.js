ba5933d7274e1a21866b5ca3c5c8762584d4b0f0
Dion Santana <dion.santana@gmail.com>
2015-05-28 11:23:43 -0400

    hurray

--- js/livet.js
+++ js/livet.js
@@ -1,18 +1,8 @@
 
-$(document).bind("mobileinit", function() {
-		        $.support.cors = true;
-		        $.mobile.allowCrossDomainPages = true;
-});
-
-$(function() {
-
-	document.addEventListener("deviceready", onDeviceReady, false);
-
-});
-
-function onDeviceReady() {
-	alert('Device ready...');
-	$( "#tour" ).popup();
-	$( "#tour" ).popup( "open" );	
+// App Logic
+function appLogic()
+{
+	
+	alert('hurray');
+	
 }
-
