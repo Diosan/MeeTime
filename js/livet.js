a1980e7994989ce7b772f49ee7c6c0390b0c1580
Dion Santana <dion.santana@gmail.com>
2015-05-29 19:08:11 -0400

    home toolbar

--- js/livet.js
+++ js/livet.js
@@ -8,6 +8,12 @@
 var gapReady = $.Deferred();
 var jqmReady = $.Deferred();
 var tourstep = 1;
+var date = new Date();
+var d = date.getDate();
+var m = date.getMonth();
+var y = date.getFullYear();
+var reading = [];
+var readingsArray = [];
 
 //Catch "deviceready" event which is fired when PhoneGap is ready
 document.addEventListener("deviceReady", deviceReady, false);
@@ -29,19 +35,35 @@ $(document).one("mobileinit", function(){
 /**
 * Run your App Logic only when both frameworks have loaded
 */
-$.when(gapReady, jqmReady).then(meeTime);
-//$.when(jqmReady).then(meeTime);
+//$.when(gapReady, jqmReady).then(meeTime);
+$.when(jqmReady).then(meeTime);
 
 // App Logic
 function meeTime()
 {
+	
+	
+	
 	setTimeout(
   		function() 
   		{
+    		//Initialize and open tour
     		$( "#tour" ).popup();
 			$( "#tour" ).popup( "open" );
+			
+			//initiate calendar widget
+		  	//$("#calendar").jqmCalendar({
+		    //    events: readingsArray,
+		    //    months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+		    //    days: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+		    //    startOfWeek: 0        
+		    //});
+    
+		    //$('#calendar').trigger('refresh');			
+			
   		}, 1000);
   		
+  	//Change content of tour popup to content for next step	
   	$('#next_tourstep').click(function(){
 		$.get( 'tour.csv', function( data ) {
 			var lines = data.split('\n');
@@ -51,6 +73,7 @@ function meeTime()
 		}, "text");  		
   	});
   	
+  	//Change content of tour popup to content for previous step
   	$('#previous_tourstep').click(function(){
 		$.get( 'tour.csv', function( data ) {
 			var lines = data.split('\n');
@@ -59,6 +82,16 @@ function meeTime()
   			tourstep--;		
 		}, "text");  		
   	});
+  	
+  	reading = { 'summary' : '<center><b>Today<b></center>', 'begin' : new Date(y, m, d), 'end' : new Date(y, m, d) };
+	readingsArray.push(reading);
+	reading = { 'summary' : '<center><b>Yesterday<b></center>', 'begin' : new Date(y, m, d - 1), 'end' : new Date(y, m, d - 1) };
+	readingsArray.push(reading);
+	reading = { 'summary' : '<center><b>Tomorrow<b></center>', 'begin' : new Date(y, m, d + 1), 'end' : new Date(y, m, d + 1) };
+	readingsArray.push(reading);
+			
+			
+  	
 	
 	
 }
